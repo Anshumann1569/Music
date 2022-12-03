139157be351391ab7b84648278b29b4387b39f1f
Anshumann1569 <119091074+Anshumann1569@users.noreply.github.com>
2022-12-03 08:21:01 +0530

    Tried To Fix

--- app.py
+++ app.py
@@ -2,8 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def hello_world():
     return "<h1>Bot Running, Made By <a href='https://telegram.me/AnshumanPM_2006'>𝑨𝒏𝒔𝒉𝒖𝒎𝒂𝒏𝑷𝑴 〄</a></h1>"
-
-if __name__ == "__main__":
-    app.run()
